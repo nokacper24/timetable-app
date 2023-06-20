8363733508143d98b20c4c208bf161d74467b0ea
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-06-21 00:46:24 +0200

    feat: :card_file_box: add db

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -1,12 +1,33 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/cloudflare-pages'
 
-const app = new Hono().basePath('/api')
+type Context = {
+  Bindings: {
+    DB: D1Database;
+  };
+  Variables: {
+    // user: User;
+  };
+};
+
+const app = new Hono<Context>().basePath('/api')
 
 app.get('/hello', (c) => {
   return c.json({
     message: 'Hello, Cloudflare Pages!',
   })
 })
+
+app.get("/users", async (c) => {
+  let user: User = await c.env.DB.prepare(
+    "SELECT * FROM users"
+  ).first();
+  return c.json(user);
+});
+
+interface User {
+  id: number;
+  username: string;
+}
 
 export const onRequest = handle(app)
