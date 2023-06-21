8e04cd46359ee8a3373d8e6ba8388bbc168b2d12
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-06-21 15:54:25 +0200

    fix: :safety_vest: move api_url to envvars

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -1,8 +1,10 @@
-interface Env {}
+interface Env {
+  API_URL: string;
+}
 
 export const onRequest: PagesFunction<Env> = async (context) => {
   let request_path = context.functionPath.slice(5);
-  const api_url = 'https://timetable-app-api.nokacper24-cfc.workers.dev/';
+  const api_url = context.env.API_URL;
   let new_url = api_url + request_path;
   return await fetch(new_url, context.request);
 };
